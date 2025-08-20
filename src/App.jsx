eaeeb47d13aaa48d3fce43429d4b953ea176569c
rohit8520 <rohitsharmashiek@gmail.com>
2025-08-20 14:36:29 +0530

    To

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+
 import TodoApp from "./Components/TodoApp";
 function App() {
   const [data, setdata] = useState([
     { id: 1, title: "Task 1" },
-    { id: 1, title: "Task 2" },
-    { id: 1, title: "Task 3" },
+    { id: 2, title: "Task 2" },
+    { id: 3, title: "Task 3" },
   ]);
-
+  const addTasks = (newTask) => {
+    setdata([...data, { id: Date.now(), ...newTask}]);
+  };
   return (
     <>
-      <TodoApp data={data} />
+      <TodoApp data={data} onAdd={addTasks} />
     </>
   );
 }
--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
 import "../style/todo.css";
-import Tolist from "./Tolist";
-const TodoApp = ({data}) => {
+const TodoApp = ({ data, onAdd }) => {
+  const [title, setTitle] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAdd({ title });
+    setTitle("");
+  };
   return (
-    <>
-      <h1>Add your task here</h1>
-      <ul className="title">
+    <div className="bg-zinc-800 h-screen w-full flex flex-col justify-center items-center gap-5">
+      <h1 className="text-4xl">Add your task here</h1>
+      <ul>
         {data.map((item) => (
-          <li key={item.id}>{item.title}</li>
+          <li className="text-white" key={item.id}>
+            {item.title}
+          </li>
         ))}
       </ul>
-      <ul></ul>
-    </>
+      <form className="flex gap-4" action="#" onSubmit={handleSubmit}>
+        <input
+          className="bg-zinc-300 px-4 py-2 rounded-md w-70"
+          type="text"
+          value={title}
+          placeholder="Add a new task"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="text-white bg-zinc-400 cursor-pointer px-4 py-2 rounded-md"
+        >
+          Add
+        </button>
+      </form>
+    </div>
   );
 };
 export default TodoApp;
